e12b939bf34a19185a76c8198da8492c6ebc940c
gabyfersan <44554967+gabyfersan@users.noreply.github.com>
2023-12-28 11:59:17 +0100

    A working GUI

--- app/sauna/page.tsx
+++ app/sauna/page.tsx
@@ -1,22 +1,27 @@
-import { Flex, Grid, Text } from "@radix-ui/themes";
+"use client";
+import { AlertDialog, Button, Flex, Grid, Text } from "@radix-ui/themes";
+import { useEffect, useState } from "react";
 
 const bookinHours = [];
 
-const getAllBookinHours = (index: number) => {
+const getAllBookinHours = (index: number): { date: string }[] => {
   const allBookinHours = [];
-  console.log(getAll(index - 1));
+  const all = getAll(index);
+  console.log("kkkk");
   for (let i = 1; i <= 25; i++) {
-    allBookinHours.push(
-      i === 1
-        ? { label: getAll(index - 1).dateFormated }
-        : {
-            label: getAll(index - 1).dateNumerical + getAllHours()[i - 1],
-            timeDate: getAll(index - 1).dateNumerical + getAllHours()[i - 1],
-          }
-    );
+    allBookinHours.push({
+      date: all.dateNumerical + "T" + allHours[i - 1],
+    });
   }
   return allBookinHours;
 };
+
+const getDateFormated = (index: number): { date: string[] } => {
+  return {
+    date: getAll(index).dateFormated,
+  };
+};
+
 const getAll = (addNumbersOfDays: number) => {
   //.toISOString().slice(0, 10);
   //   const dateNow = new Date();
@@ -45,7 +50,12 @@ const getAll = (addNumbersOfDays: number) => {
 
   date.setDate(date.getDate());
   //date.toISOString().slice(0, 11);
-  const dateNumerical = date.toISOString().slice(0, 11);
+  //const dateNumerical = date.toISOString().slice(0, 11);
+  const dateNumerical = new Intl.DateTimeFormat("sv-SE", {
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+  }).format(date);
   return { dateFormated, dateNumerical };
   // const options = {
   //   weekday: "long",
@@ -71,93 +81,186 @@ const getAllHours = () => {
   return hours;
 };
 
+const allHours = getAllHours();
+
+const moveToClosetsHour = (time: string) => {
+  const newTime = new Date(time);
+
+  return newTime.setHours(newTime.getHours() + 1);
+};
+
 let base = {
-  height: "5em",
-  border: "2px solid #ffa94d",
-  "border-radius": "5px",
-  color: "#d9480f",
-  "text-align": "center",
+  //border: "2px solid #ffa94d",
+  //"border-radius": "5px",
+  //color: "#d9480f",
+  //"text-align": "center",
+  //"box-sizing": "border-box;",
 };
 
 let styleForTime = {
+  "background-color": "#ffd8a8",
   ...base,
   width: "4.5em",
-  "background-color": "#ffd8a8",
+  height: "3em",
+  //"background-color": "#ffd8a8",
   margin: "2px 2px 2px 0",
   overflow: "clip",
 };
 
 let styleForBooking = {
   ...base,
-  width: "6em",
-  "background-color": "green",
+  //width: "6em",
+  //height: "4.2em",
+  //"background-color": "green",
   margin: "2px",
   //"z-index": "-1",
 };
 
 let styleForGrid = {
-  "grid-template-columns":
-    "1fr 1fr 1fr 1fr  1fr  1fr  1fr 1fr 1fr  1fr  1fr  1fr",
+  gridTemplateColumns: "1fr 1fr 1fr 1fr  1fr  1fr  1fr 1fr 1fr  1fr  1fr  1fr",
   //  "position": "fixed"
   //contain: "paint",
-  height: "600px",
+  height: "500px",
   width: "100%",
   overflow: "auto",
 };
 
 const Sauna = () => {
-  const allHours = getAllHours();
-  console.log();
+  const bookSauna = (e: any) => {
+    console.log(e.target);
+    console.log(e.target.dataset.dateAndTime);
+    setShowDialog(true);
+  };
+  const [showDialog, setShowDialog] = useState(false);
+  useEffect(() => {
+    // if (
+    //   document &&
+    //   document.getElementById("grid") &&
+    //   document.getElementById("grid").style &&
+    //   document.getElementById("grid").style.height
+    // ) {
+    //   document.getElementById("grid").style.height =
+    //     innerHeight -
+    //     document.getElementById("grid").getBoundingClientRect().top -
+    //     30 +
+    //     "px";
+    // }
+  }, []);
+  //const allHours = getAllHours();
+  //console.log(allHours);
+  let getAllBookinHoursDayAndI: { date: string }[];
   return (
-    <Grid style={styleForGrid}>
-      <Flex
-        direction='column'
-        // gap='0'
-        className=' sticky left-0 z-10 bg-white'
-      >
-        {allHours.map((a, i) =>
-          i === 0 ? (
-            <div
-              style={{ width: "4.5em", height: "5em", margin: "2px 2px 2px 0" }}
-            ></div>
-          ) : (
-            <div
-              key={a}
-              style={styleForTime}
-              className={`flex items-center justify-center sticky left-0`}
-            >
-              <Text size='6'>{a}</Text>
-            </div>
-          )
-        )}
-      </Flex>
-      {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) => (
-        <Flex direction='column' key={i}>
-          {getAllBookinHours(i).map((a, ii) => (
-            <div
-              key={Array.isArray(a.label) ? a.label[1] : a.label}
-              style={styleForBooking}
-              className={`flex items-center justify-center   	flex-col ${
-                ii === 0 ? " !bg-white sticky top-0" : ""
-              }`}
-            >
-              {ii === 0 ? (
-                <>
-                  <Text size='3' className='block'>
-                    {a?.label![0]}
-                  </Text>
-
-                  <Text size='3'> {a?.label![1] + " " + a?.label![2]}</Text>
-                </>
-              ) : (
-                <Text size='1'>{a.label}</Text>
-              )}
-              <Text size='6'></Text>
-            </div>
-          ))}
+    <>
+      {" "}
+      <AlertDialog.Root open={showDialog}>
+        <AlertDialog.Content>
+          <AlertDialog.Title>Error</AlertDialog.Title>
+          <AlertDialog.Description>
+            This issue could not be deleted.
+          </AlertDialog.Description>
+          <Button
+            color='gray'
+            variant='soft'
+            mt='2'
+            onClick={() => setShowDialog(false)}
+          >
+            OK
+          </Button>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+      <Grid style={styleForGrid} id='grid'>
+        <Flex
+          direction='column'
+          // gap='0'
+          className=' sticky left-0  bg-white'
+          style={{
+            zIndex: 10,
+          }}
+        >
+          {allHours.map((a, i) =>
+            i === 0 ? (
+              <div
+                key='jjj'
+                style={{
+                  width: "4.5em",
+                  height: "3em",
+                  margin: "2px 2px 2px 0",
+                }}
+              ></div>
+            ) : (
+              <div
+                key={a}
+                style={{
+                  width: "4.5em",
+                  height: "3em",
+                  margin: "2px 2px 2px 0",
+                  borderRadius: "5px",
+                  backgroundColor: "#ffd8a8",
+                  overflow: "clip",
+                  zIndex: 99,
+                }}
+                className={`flex items-center justify-center sticky left-0 `}
+              >
+                <Text size='6'>{a}</Text>
+              </div>
+            )
+          )}
         </Flex>
-      ))}
-    </Grid>
+        {Array.from(Array(10).keys()).map((day) => {
+          getAllBookinHoursDayAndI = getAllBookinHours(day);
+          return (
+            <Flex direction='column' key={day} onClick={bookSauna}>
+              {Array.from(Array(25).keys()).map((i) =>
+                i === 0 ? (
+                  <div
+                    key={getDateFormated(day).date[1]}
+                    style={{
+                      margin: "2px",
+                      borderRadius: "5px",
+                      backgroundColor: "#ffd8a8",
+                      opacity: 1,
+                      zIndex: 1,
+                    }}
+                    className={`flex items-center justify-center flex-col sticky top-0  `}
+                  >
+                    <Text size='3' className='block'>
+                      {getDateFormated(day).date[0]}
+                    </Text>
+                    <Text size='3'>
+                      {" "}
+                      {getDateFormated(day).date[1] +
+                        " " +
+                        getDateFormated(day).date[2]}
+                    </Text>
+                  </div>
+                ) : (
+                  <Button
+                    data-te-ripple-init
+                    data-te-ripple-unbound='true'
+                    color='lime'
+                    variant='classic'
+                    size='4'
+                    disabled={
+                      new Date().getTime() >
+                      moveToClosetsHour(getAllBookinHoursDayAndI[i].date)
+                    }
+                    data-date-and-time={getAllBookinHoursDayAndI[i].date}
+                    key={getAllBookinHoursDayAndI[i].date}
+                    style={{
+                      margin: "2px 2px 2px 2px",
+                      // zIndex: -2,
+                    }}
+                    className={`flex items-center justify-center flex-col active:bg-lime-100`}
+                  >
+                    <Text size='2'>{getAllBookinHoursDayAndI[i].date}</Text>
+                  </Button>
+                )
+              )}
+            </Flex>
+          );
+        })}
+      </Grid>
+    </>
   );
 };
 
