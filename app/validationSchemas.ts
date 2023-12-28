707ed990322838359d4b46b5b4a60d80d630b718
gabyfersan <44554967+gabyfersan@users.noreply.github.com>
2023-12-29 00:46:56 +0100

    Add sauna model and validation

--- app/validationSchemas.ts
+++ app/validationSchemas.ts
@@ -1,5 +1,20 @@
 import { z } from "zod";
 
+const dateRegex =
+  /^(\d{4})-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])T([01][0-9]|2[0-3]):00$/;
+
+export const saunaSchema = z.object({
+  bookedAtDateAndTime: z.string().refine((val) => dateRegex.test(val), {
+    message: "Invalid date format",
+  }),
+  message: z.string().min(0).max(255, "Längden ska vara max vara 65535 tecken").optional(),
+  shareSauna: z.boolean().optional(),
+  bookedByUserId: z
+  .string()
+  .min(1, "Längden ska vara minst vara 1 tecken")
+  .max(255, "Längden ska vara max vara 255 tecken")
+});
+
 export const issueSchema = z.object({
   title: z
     .string()
