4f58c694779b0724208da41f7253203b69faa3bb
gabyfersan <44554967+gabyfersan@users.noreply.github.com>
2024-01-10 22:24:40 +0100

    Remove unsused code

--- app/auth/authOptions.ts
+++ app/auth/authOptions.ts
@@ -36,38 +36,48 @@ const authOptions: NextAuthOptions = {
         return passwordMatch ? user : null;
       },
     }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email", placeholder: "Postadress" },
+        password: {
+          label: "Lösenord",
+          type: "password",
+          placeholder: "Lösenord",
+        },
+      },
+      async authorize(credentials, req) {
+        if (!credentials!.email || !credentials!.password) {
+          return null;
+        }
+        const user = await prisma.user.findUnique({
+          where: { email: credentials!.email },
+        });
+        if (!user) {
+          return null;
+        }
+
+        const passwordMatch = await bcrypt.compare(
+          credentials!.password,
+          user.hashedPassword!
+        );
+        return passwordMatch ? user : null;
+      },
+    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
   session: { strategy: "jwt" },
-  // callbacks: {
-  //   session: async ({ session, user }) => {
-  //     // Fetch additional user data from your database
-  //     const userData = await prisma.user.findUnique({
-  //       where: { email: user.email }, // Assuming user data is identified by email
-  //       select: { id: true }, // Select the fields you need (e.g., id)
-  //     });
-  //     console.log("session auth", session, userData);
-  //     if (userData) {
-  //       session.user.id = userData.id; // Add the id to the session object
-  //     }
 
-  //     return session;
-  //   },
-  // },
   callbacks: {
     session: async ({ session, token, user }: any) => {
-      // console.log("SESSION", session);
-      // console.log("USER", user);
-      //console.log("token", token);
       if (session?.user?.email) {
         const userData = await prisma.user.findUnique({
           where: { email: session.user.email }, // Assuming user data is identified by email
           select: { id: true }, // Select the fields you need (e.g., id)
         });
-        //console.log("session auth", session, userData);
         if (userData) {
           session.user.id = userData.id; // Add the id to the session object
         }
