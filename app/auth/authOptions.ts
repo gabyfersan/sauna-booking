66322848eb9dc4ca61d148a39b8c043faded23e9
gabyfersan <44554967+gabyfersan@users.noreply.github.com>
2024-01-11 18:49:04 +0100

    Change so only email login i possible

--- app/auth/authOptions.ts
+++ app/auth/authOptions.ts
@@ -3,7 +3,6 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import bcrypt from "bcrypt";
 import { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-import GoogleProvider from "next-auth/providers/google";
 
 const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
@@ -36,38 +35,11 @@ const authOptions: NextAuthOptions = {
         return passwordMatch ? user : null;
       },
     }),
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        email: { label: "Email", type: "email", placeholder: "Postadress" },
-        password: {
-          label: "Lösenord",
-          type: "password",
-          placeholder: "Lösenord",
-        },
-      },
-      async authorize(credentials, req) {
-        if (!credentials!.email || !credentials!.password) {
-          return null;
-        }
-        const user = await prisma.user.findUnique({
-          where: { email: credentials!.email },
-        });
-        if (!user) {
-          return null;
-        }
 
-        const passwordMatch = await bcrypt.compare(
-          credentials!.password,
-          user.hashedPassword!
-        );
-        return passwordMatch ? user : null;
-      },
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
+    // GoogleProvider({
+    //   clientId: process.env.GOOGLE_CLIENT_ID!,
+    //   clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    // }),
   ],
   session: { strategy: "jwt" },
 
