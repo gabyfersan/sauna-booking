2b95bddab14aa4586da19e1a04d964e1ca9c4f25
gabyfersan <44554967+gabyfersan@users.noreply.github.com>
2024-01-17 18:26:24 +0100

    Show nbs flag when not logged in

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import prisma from "@/prisma/client";
 import { Flex, Grid } from "@radix-ui/themes";
 import { Metadata } from "next";
+import { getServerSession } from "next-auth";
 import IssueChart from "./IssueChart";
 import IssueSummary from "./IssueSummary";
 import LatestIssues from "./LatestIssues";
@@ -8,11 +9,21 @@ import LatestIssues from "./LatestIssues";
 export const dynamic = "force-dynamic";
 
 export default async function Home() {
+  const session = await getServerSession();
   const open = await prisma.issue.count({ where: { status: "OPEN" } });
   const inProgress = await prisma.issue.count({
     where: { status: "IN_PROGRESS" },
   });
   const closed = await prisma.issue.count({ where: { status: "CLOSED" } });
+
+  if (!session) {
+    return (
+      <Flex className='justify-center'>
+        <img src='/nbs.jpeg' />
+      </Flex>
+    );
+  }
+
   return (
     <Grid columns={{ initial: "1", md: "2" }} gap='5'>
       <Flex direction='column' gap='5'>
